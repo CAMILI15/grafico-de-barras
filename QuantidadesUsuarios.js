3f29240c1176adb31d1cfdb3a3b0a9ea5c661490
CAMILI15 <104512708+CAMILI15@users.noreply.github.com>
2024-10-24 11:31:37 -0300

    Add files via upload

---
+++ QuantidadesUsuarios.js
@@ -0,0 +1,30 @@
+import { getCSS} from "./common.js"
+
+async fuction quantidadeUsuarios(){
+    const url ='https://raw.guthubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeUsuarios,
+            type: 'bar' ,
+            marker:{
+                color: getCSS('--primary-color') 
+                
+            }
+        }
+    ]
+const layout = {
+plot_bgcolor: getCSS('--bg-color')
+}
+    const grafico = document.createElement(div)
+    grafico.className ='grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+}
+
+quantidadeUsuarios()
